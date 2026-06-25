5284ff24038e69aeaff4c16625059e8e9312f201
satheeshMsakkaa <139038595+satheeshMsakkaa@users.noreply.github.com>
2026-06-26 00:25:52 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://backend-4bx3.onrender.com"
+  baseURL: "https://backend-4bx3.onrender.com/api"
 });
 
 export default API;
